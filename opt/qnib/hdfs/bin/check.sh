1c668e97cf8d1a693b46a9b7e7e4c5e68b0bc898
Christian Kniep <christian@qnib.org>
2016-09-19 15:11:00 -0700

    check for open|filtered

--- opt/qnib/hdfs/bin/check.sh
+++ opt/qnib/hdfs/bin/check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 IP_ADD=$(ip -o -4 address |grep eth0 |egrep -o "\d+\.\d+\.\d+\.\d+")
-echo "Check for open '$1' (port: $2)"
+echo "Check for open|filtered '$1' (port: $2)"
 nmap ${IP_ADD} -p $2 
-nmap ${IP_ADD} -p $2 |grep open
+nmap ${IP_ADD} -p $2 |egrep "(open|filtered)"
 exit $?
