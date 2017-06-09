b09ac640bb633cc83f6c447129f69c5d8486b46c
Christian Kniep <christian@qnib.org>
2017-06-09 11:28:10 +0200

    adjust NODE_IP parsing

--- opt/qnib/hdfs/namenode/bin/configure.sh
+++ opt/qnib/hdfs/namenode/bin/configure.sh
@@ -4,7 +4,7 @@ source /opt/qnib/consul/etc/bash_functions.sh
 wait_for_srv consul-http
 sleep 2
 
-export NODE_IP=$(getent hosts $(go-getmyname) |awk '{print $1}')
+export NODE_IP=$(ip route |awk '/eth0\s+src/{print $NF}')
 
 consul-template -consul localhost:8500 -once -template "/etc/consul-templates/hdfs/namenode/hdfs-site.xml.ctmpl:/opt/hadoop/etc/hadoop/hdfs-site.xml"
 consul-template -consul localhost:8500 -once -template "/etc/consul-templates/hdfs/core-site.xml.ctmpl:/opt/hadoop/etc/hadoop/core-site.xml"
